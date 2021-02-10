2b2e02ab132b3cf48248ba5a4fc12c6fc59a42fe
alayzcer <blaze48@mail.ru>
2021-02-10 22:09:26 +0300

    some packages were added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
+
+
 import _ from "lodash";
 
-_.intersection([2, 1], [2, 3]); 
+_.intersection([2, 1], [2, 3]);
 _.capitalize("hello");
 
-
-console.log('Hello, Hexlet!');
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet!");
+console.log(_.last(["one", "two"]));
